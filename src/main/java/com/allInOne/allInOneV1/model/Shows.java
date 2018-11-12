671818d256277f225f6e5a24f8bff6fc643fd7a7
Priyanka Nagaraju <priyanka@Priyankas-MacBook-Pro.local>
2018-11-12 17:39:40 -0500

    Movie Screenings model

---
+++ src/main/java/com/allInOne/allInOneV1/model/Shows.java
@@ -0,0 +1,41 @@
+package com.allInOne.allInOneV1.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "Shows")
+public class Shows {
+
+    @EmbeddedId
+    private MovieTheatre movieTheatre;
+
+    private Date showTime;
+
+    public MovieTheatre getMovieTheatre() {
+        return movieTheatre;
+    }
+
+    public void setMovieTheatre(MovieTheatre movieTheatre) {
+        this.movieTheatre = movieTheatre;
+    }
+
+    public Date getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(Date showTime) {
+        this.showTime = showTime;
+    }
+
+    public Shows() {
+
+    }
+
+    public Shows(MovieTheatre movieTheatre, Date showTime) {
+        this.movieTheatre = movieTheatre;
+        this.showTime = showTime;
+    }
+
+}
+
--- src/main/java/com/allInOne/allInOneV1/repository/MovieRepository.java
+++ src/main/java/com/allInOne/allInOneV1/repository/MovieRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface MovieRepository extends JpaRepository <Movie, Integer>, MovieRepositoryCustom{
+public interface MovieRepository extends JpaRepository <Movie, Integer>, MovieRepositoryCustom {
 
 }
