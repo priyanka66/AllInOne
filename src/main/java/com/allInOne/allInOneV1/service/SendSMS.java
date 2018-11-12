49dc1d0dcb967df4288434ba75cc06ca9938a999
Priyanka Nagaraju <priyanka@Priyankas-MBP.fios-router.home>
2018-11-11 21:17:37 -0500

    Removed security keys

--- src/main/java/com/allInOne/allInOneV1/service/SendSMS.java
+++ src/main/java/com/allInOne/allInOneV1/service/SendSMS.java
@@ -7,8 +7,8 @@ import com.twilio.type.PhoneNumber;
 import java.util.HashMap;
 
 public class SendSMS {
-    public static final String ACCOUNT_SID = "ACd304487421fb1cae7c9b3de98ba4aac3";
-    public static final String AUTH_TOKEN = "000749e7f963f51bb8a245028c2731a7";
+    public static final String ACCOUNT_SID = "xyz";
+    public static final String AUTH_TOKEN = "xyz";
     HashMap<String, String> messageTemplate = new HashMap<String, String>();
 
     public void sendSMS(String receiver, String sender) {
